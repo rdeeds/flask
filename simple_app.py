9e5ffb66ca198ae59e318a07a188a85f254d119e
Ryan Deeds <ryancdeeds@gmail.com>
2016-10-02 17:15:33 -0500

    initial commit

--- simple_app.py
+++ simple_app.py
@@ -2,25 +2,24 @@ from flask import Flask
 from flask import request
 from flask import render_template
 
-app= Flask(__name__)
+app = Flask(__name__)
+
 
 @app.route('/')
 @app.route('/<name>')
 def index(name="Treehouse"):
-    return render_template('index.html',name=name)
+    return render_template('index.html', name=name)
+
 
 @app.route('/add/<int:num1>/<int:num2>')
 def add(num1, num2):
-    return render_template('add.html', num1=num1,num2=num2)
-
-    #.format(num1,num2,num1+num2)
+    return render_template('add.html', num1=num1, num2=num2)
 
 
 @app.route('/multiply')
 @app.route('/multiply/<int:num1>/<int:num2>')
-def multiply(num1=5,num2=5):
-    return str(num1*num2)
-
+def multiply(num1=5, num2=5):
+    return str(num1 * num2)
 
 
 app.run(debug=True, port=8000, host='0.0.0.0')
