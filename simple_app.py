03fc78b1a9345212c4cbe447640e1d82202a4a63
Ryan Deeds <ryancdeeds@gmail.com>
2016-10-02 19:18:58 -0500

    initial commit

--- simple_app.py
+++ simple_app.py
@@ -1,14 +1,32 @@
-from flask import Flask,request, render_template,redirect,url_for
+from flask import (Flask, request, render_template,
+                   redirect, url_for, make_response)
+import json
 
 app = Flask(__name__)
 
+
+def get_saved_data():
+    try:
+        data = json.loads(request.cookies.get('character'))
+    except TypeError:
+        data = {}
+    return data
+
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    data = get_saved_data()
+    return render_template('index.html', saves=data)
 
-@app.route('/save',methods=['POST'])
+
+@app.route('/save', methods=['POST'])
 def save():
-    import pdb; pdb.set_trace()
-    return redirect(url_for('index'))
+    response = make_response(redirect((url_for('index'))))
+    #   import pdb; pdb.set_trace()
+    data = get_saved_data()
+    data.update(dict(request.form.items()))
+    response.set_cookie('character', json.dumps(data))
+    return response
+
 
 app.run(debug=True, port=8000, host='0.0.0.0')
